997465c03f528b592bfa28c2173d2cf1c016026c
Sahishnu <sahishnuk2@gmail.com>
2025-07-03 17:48:29 +0800

    Added more functionality to task manager such as filter, sort (without button)

--- src/main/java/com/example/chronopanthers/ChronoPanthers.java
+++ src/main/java/com/example/chronopanthers/ChronoPanthers.java
@@ -12,11 +12,11 @@ import java.io.IOException;
 public class ChronoPanthers extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(ChronoPanthers.class.getResource("loginPage.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(ChronoPanthers.class.getResource("taskManager.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChronoPanthers.class.getResource("loginPage.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(ChronoPanthers.class.getResource("taskManager.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        //scene.getStylesheets().add(getClass().getResource("/com/example/chronopanthers/loginPage.css").toExternalForm());
-        scene.getStylesheets().add(getClass().getResource("/com/example/chronopanthers/taskManager.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/com/example/chronopanthers/loginPage.css").toExternalForm());
+        //scene.getStylesheets().add(getClass().getResource("/com/example/chronopanthers/taskManager.css").toExternalForm());
         stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
